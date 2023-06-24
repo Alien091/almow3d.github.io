56c5c407a4581b80353daf3643b656590abc5091
Alien091 <135403395+Alien091@users.noreply.github.com>
2023-06-24 21:22:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -65,24 +65,19 @@ header.classList.toggle('sticky', window.scrollY > 100);
 
    /* ========= swiper photo =============*/
 
-    const swiper = new Swiper('.swiper', {
-      // Optional parameters
-      direction: 'horizontal',
-      loop: true,
-    
-      // If we need pagination
-      // pagination: {
-      //   el: '.swiper-pagination',
-      // },
-    
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    
-      // And if we need scrollbar
-      scrollbar: {
-        el: '.swiper-scrollbar',
-      },
-    });
+   var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
